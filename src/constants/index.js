32617ead36eedfd2fb15a9387183438dc951de5a
RustamSGP <sgpgamesnft@gmail.com>
2024-02-07 19:35:33 +0200

    server4

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 //const API_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:3000";
-const API_URL = process.env.REACT_APP_BACKEND_URL || "https://dog-sooty-seven.vercel.app";
+const API_URL = "https://dog-sooty-seven.vercel.app";
 const USERS_API_URL = `${API_URL}/users`;
 
 export { API_URL, USERS_API_URL };
